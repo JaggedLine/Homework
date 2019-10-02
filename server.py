557724c125c8e544e60f12684fe8e261d0c4afbb
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2019-10-02 22:37:24 +0300

    Working on server

--- server.py
+++ server.py
@@ -2,6 +2,24 @@ from http.server import BaseHTTPRequestHandler,HTTPServer
 
 PORT_NUMBER = 5000
 
+class SecurityError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+def process(data: str) -> list:
+    return [(0, 0)]
+
+def clac(data: str) -> int:
+    line = process(data)
+    if len(set(line)) != len(line):
+        raise SecurityError("STOP CHEATING! BAN!")
+    if 
+
+def save_results(result: tuple, field_size: int) -> None:
+    f = open("table_" + str(field_size) + ".json", 'a')
+    f.write(result[0] + ':' + str(result[1]) + ',')
+    f.close()
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/' or self.path == "":
@@ -39,10 +57,20 @@ class Handler(BaseHTTPRequestHandler):
                 f.close()
             return
         except IOError:
-            self.send_error(404,"File Not Found: " + self.path)
+            self.send_error(404, "File Not Found: " + self.path)
 
     def do_POST(self):
-        pass
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length).decode("utf8")
+        print(post_data)
+        try:
+            result = calc(post_data)
+        except SecurityError as e:
+            self.send_error(403, e.message)
+        save_results((self.headers["Name"], result), int(self.headers["Field-Size"]))
+        self.send_response(200)
+        self.send_header("Content-type", "")
+        self.end_headers()
 
 try:
     server = HTTPServer(("", PORT_NUMBER), Handler)

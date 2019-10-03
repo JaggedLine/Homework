72c5b8fd96ea1a6864dd400770b7e4c25227f33e
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2019-10-03 16:47:22 +0300

    Now we have data

--- submit.js
+++ submit.js
@@ -1,17 +1,18 @@
 async function sendJSON() {
-	let response = await fetch("/results.json", 
-		{
-			method: 'post',
-			headers: {
-				"Content-Type": "application/json;charset=utf-8",
-				"Field-Size": "6",
-				"Name": document.getElementById('name').value
-			},
-			body: data.getAttribute('points')
-		})
+    let response = await fetch("/results.json", 
+        {
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+                "Field-Size-X": "6",
+                "Field-Size-Y": "6",
+                "Name": document.getElementById('name').value
+            },
+            body: data.getAttribute('points')
+        })
 }
 
 console.log(document.getElementById('name'), document.getElementById('name')
-	.value);
+    .value);
 submit_button.addEventListener('click', function() {console.log('submit'); sendJSON();});
 

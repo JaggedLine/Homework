af122237739c6b862be74dd0a572d42562b93e31
Ivan Gaidai-Turlov <43723504+gtivansan@users.noreply.github.com>
2019-10-03 16:03:58 +0300

    Create submit.js

---
+++ submit.js
@@ -0,0 +1,17 @@
+async function sendJSON() {
+	let response = await fetch("/results.json", 
+		{
+			method: 'post',
+			headers: {
+				"Content-Type": "application/json;charset=utf-8",
+				"Field-Size": "6",
+				"Name": document.getElementById('name').value
+			},
+			body: data.getAttribute('points')
+		})
+}
+
+console.log(document.getElementById('name'), document.getElementById('name')
+	.value);
+submit_button.addEventListener('click', function() {console.log('submit'); sendJSON();});
+

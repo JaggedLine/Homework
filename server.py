15315f139899bad3a13dead4511f49d36a90469d
Ivan Gaidai-Turlov <43723504+gtivansan@users.noreply.github.com>
2019-10-03 16:02:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -13,12 +13,12 @@ def vec_mul(a: tuple, b: tuple) -> tuple:
     return a[0] * b[1] - a[1] * b[0]
 
 def intersect(segment1: tuple, segment2: tuple) -> bool:
-    x1, y1, x2, y2, x3, y3, x4, y4 = segment1[0][0], segment1[0][1], segment1[1][0], segment1[1][1], segment2[0][0], segment2[0][1], segment2[1], segment2[1]
-    a = vec_mul(x2 - x1, y2 - y1, x3 - x1, y3 - y1) * vec_mul(x2 - x1, y2 - y1, x4 - x1, y4 - y1);
-    b = vec_mul(x4 - x3, y4 - y3, x1 - x3, y1 - y3) * vec_mul(x4 - x3, y4 - y3, x2 - x3, y2 - y3);
+    x1, y1, x2, y2, x3, y3, x4, y4 = segment1[0][0], segment1[0][1], segment1[1][0], segment1[1][1], segment2[0][0], segment2[0][1], segment2[1][0], segment2[1][1]
+    a = vec_mul((x2 - x1, y2 - y1), (x3 - x1, y3 - y1)) * vec_mul((x2 - x1, y2 - y1), (x4 - x1, y4 - y1));
+    b = vec_mul((x4 - x3, y4 - y3), (x1 - x3, y1 - y3)) * vec_mul((x4 - x3, y4 - y3), (x2 - x3, y2 - y3));
     return (a <= 0 and b <= 0)
 
-def clac(data: str, field_size: int) -> int:
+def calc(data: str, field_size: int) -> int:
     line, x, y = process(data)
     if (len(set(line)) != len(line)):
         raise SecurityError("STOP CHEATING! BAN!")
@@ -28,7 +28,7 @@ def clac(data: str, field_size: int) -> int:
         for j in range(i+2, len(line)):
             if (intersect((line[i-1], line[i]), (line[j-1], line[j]))):
                 raise SecurityError("STOP CHEATING! BAN!")
-    return len(line)
+    return len(line) - 1
 
 def save_results(result: tuple, field_size: int) -> None:
     f = open("table_" + str(field_size) + ".json", 'a')
@@ -79,10 +79,10 @@ class Handler(BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length).decode("utf8")
         print(post_data)
         try:
-            result = calc(post_data)
+            result = calc(post_data, int(self.headers["Field-Size"]))
         except SecurityError as e:
             self.send_error(403, e.message)
-        save_results((self.headers["Name"], result), int(self.headers["Field-Size"]))
+        # save_results((self.headers["Name"], result), int(self.headers["Field-Size"]))
         self.send_response(200)
         self.send_header("Content-type", "")
         self.end_headers()
